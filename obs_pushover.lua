6c64cb77c418489418649ea88883f6cb0a27be8a
b1scu17 <github@b1scu17.xyz>
2021-09-12 12:02:06 +0200

    Update obs_pushover.lua

--- obs_pushover.lua
+++ obs_pushover.lua
@@ -20,7 +20,6 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 --
--- Inspired by script developed by Mark Lagendijk
 -- Released on GitHub by Ruvann Beukes
 
 obs = obslua
